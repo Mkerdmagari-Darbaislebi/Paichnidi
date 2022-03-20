2ecdc5978b6a49d79a6fffd6ab4f28fa3054d4bc
samKHa92 <samkha@gfmstudio.com>
2022-03-20 14:45:44 +0400

    Implement Input class with static boolean keyArray

--- src/main/java/Input.java
+++ src/main/java/Input.java
@@ -1,2 +1,31 @@
-public class Input {
+import org.lwjgl.glfw.GLFWKeyCallback;
+import static org.lwjgl.glfw.GLFW.*;
+
+
+public class Input extends GLFWKeyCallback{
+
+    public static boolean[] keys = new boolean[65535];
+
+    @Override
+    public void invoke(long window, int key, int scancode, int action, int mods) {
+        keys[key] = action != GLFW_RELEASE;
+    }
+
+    //LUKA, DONT FORGET THIS!
+
+    //inside main update() method:
+    //  glfwPollEvents();
+    //  if(Input.keys[GLFW_KEY_SPACE])
+    //      {whatever happens after pressing SPACE}
+
+    //at the end of main run() method:
+    //  keyCallback.release();
+
+
+
+
+
+
+
+
 }
