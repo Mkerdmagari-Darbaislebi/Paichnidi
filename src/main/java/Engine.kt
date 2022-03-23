5c6347f34bcf63081ef0c98916cab4cc71adcd0c
YamamotoTsunetomo <arlongtwentylemons@gmail.com>
2022-03-23 21:02:15 +0400

    move GLFW procedures of Engine to Window class

--- src/main/java/Engine.kt
+++ src/main/java/Engine.kt
@@ -1,16 +1,12 @@
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import util.Time
 import kotlinx.coroutines.runBlocking
 import org.lwjgl.Version
-import org.lwjgl.glfw.Callbacks
 import org.lwjgl.glfw.GLFW
-import org.lwjgl.glfw.GLFWErrorCallback
 import org.lwjgl.opengl.GL
 import org.lwjgl.opengl.GL11
 import org.lwjgl.system.MemoryStack
-import org.lwjgl.system.MemoryUtil
-import org.lwjgl.system.SharedLibrary.Default
+import util.Time
 
 const val WIDTH: Int = 300
 const val HEIGHT: Int = 300
@@ -26,25 +22,14 @@ class Engine {
 
     private var UPS: Double = 0.0
 
-//    private var EngineThread: Thread = Thread(Runnable { run() })
+//    private var EngineThread: Thread = Thread { run() }
 
     private fun run() {
-        println("Hello LWJGL " + Version.getVersion() + "!")
+        println("Hello LWJGL ${Version.getVersion()}!")
         init()
         GL.createCapabilities()
         GL11.glClearColor(1.0f, 0.0f, 0.0f, 0.0f)
         loop()
-    }
-
-    fun stop() {
-        running = false
-        // Free the window callbacks and destroy the window
-        Callbacks.glfwFreeCallbacks(mainWindow.window)
-        GLFW.glfwDestroyWindow(mainWindow.window)
-
-        // Terminate GLFW and free the error callback
-        GLFW.glfwTerminate()
-        GLFW.glfwSetErrorCallback(null)!!.free()
     }
 
     fun start() {
@@ -52,23 +37,15 @@ class Engine {
         run()
     }
 
-
     private fun init() {
-        // Setup an error callback. The default implementation
-        // will print the error message in System.err.
-        GLFWErrorCallback.createPrint(System.err).set()
-
-        // Initialize GLFW. Most GLFW functions will not work before doing this.
-        check(GLFW.glfwInit()) { "Unable to initialize GLFW" }
+        // Setup an error callback
+        Window.setupErrorCallback(System.err)
 
         // Configure GLFW
-        GLFW.glfwDefaultWindowHints() // optional, the current window hints are already the default
-        GLFW.glfwWindowHint(GLFW.GLFW_VISIBLE, GLFW.GLFW_FALSE) // the window will stay hidden after creation
-        GLFW.glfwWindowHint(GLFW.GLFW_RESIZABLE, GLFW.GLFW_TRUE) // the window will be resizable
+        mainWindow.configure()
 
         // Create the window
         mainWindow.createWindow()
-        if (mainWindow.window == MemoryUtil.NULL) throw RuntimeException("Failed to create the GLFW window")
 
         // Setup a key callback. It will be called every time a key is pressed, repeated or released.
         GLFW.glfwSetKeyCallback(
@@ -79,38 +56,35 @@ class Engine {
                 true
             )
         }
+
         MemoryStack.stackPush().also { stack ->
             val pWidth = stack.mallocInt(1) // int*
             val pHeight = stack.mallocInt(1) // int*
 
             // Get the window size passed to glfwCreateWindow
-            GLFW.glfwGetWindowSize(mainWindow.window, pWidth, pHeight)
-
-            // Get the resolution of the primary monitor
-            val vidmode = GLFW.glfwGetVideoMode(GLFW.glfwGetPrimaryMonitor())
+            mainWindow.getWindowSize(pWidth, pHeight)
 
             // Center the window
-            GLFW.glfwSetWindowPos(
-                mainWindow.window,
-                (vidmode!!.width() - pWidth[0]) / 2,
-                (vidmode.height() - pHeight[0]) / 2
-            )
+            mainWindow.centerWindow(pWidth, pHeight)
         }
 
         // Make the OpenGL context current
-        GLFW.glfwMakeContextCurrent(mainWindow.window)
+        mainWindow.makeContextCurrent()
+
         // Enable v-sync
-        GLFW.glfwSwapInterval(1)
+        mainWindow.enableVSync()
 
         // Make the window visible
-        GLFW.glfwShowWindow(mainWindow.window)
+        mainWindow.showWindow()
     }
 
     private fun loop() {
         while (running) {
             var passedTime: Double = Time.currentTime() - lastUPS
             var renderLock: Boolean = false
-            if (GLFW.glfwWindowShouldClose(mainWindow.window)) stop()
+            if (mainWindow.windowShouldClose())
+                mainWindow.stop()
+
             if (System.nanoTime() - lastFPSUPSout > 1000000000) {
                 println("FPS: $UPS")
                 UPS = 0.0
--- src/main/java/Window.kt
+++ src/main/java/Window.kt
@@ -1,6 +1,11 @@
+import org.lwjgl.glfw.Callbacks
 import org.lwjgl.glfw.GLFW
+import org.lwjgl.glfw.GLFWErrorCallback
 import org.lwjgl.opengl.GL11
+import org.lwjgl.system.Callback
 import org.lwjgl.system.MemoryUtil
+import java.io.PrintStream
+import java.nio.IntBuffer
 
 class Window(
     private val WIDTH: Int,
@@ -9,8 +14,13 @@ class Window(
 ) {
     var window = 0L
 
+    init {
+        check(GLFW.glfwInit()) { "Unable to initialize" }
+    }
+
     fun createWindow() {
         window = GLFW.glfwCreateWindow(WIDTH, HEIGHT, TITLE, MemoryUtil.NULL, MemoryUtil.NULL)
+        if (window == MemoryUtil.NULL) throw RuntimeException("Failed to create window!")
     }
 
     fun render() = GLFW.glfwSwapBuffers(window)
@@ -18,4 +28,62 @@ class Window(
     fun update() = GL11.glClear(GL11.GL_COLOR_BUFFER_BIT or GL11.GL_DEPTH_BUFFER_BIT)
 
     fun clean() = GLFW.glfwPollEvents()
+
+    fun stop() {
+        freeCallbacks()
+        destroyWindow()
+        terminate()
+    }
+
+    fun centerWindow(width: IntBuffer, height: IntBuffer) {
+        val videoMode = getVideoMode()
+        videoMode?.let { vm ->
+            setWindowPos(
+                (vm.width() - width[0]) / 2,
+                (vm.height() - height[0]) / 2
+            )
+        }
+    }
+
+    fun getWindowSize(
+        width: IntBuffer,
+        height: IntBuffer,
+    ) = GLFW.glfwGetWindowSize(window, width, height)
+
+    fun windowShouldClose() = GLFW.glfwWindowShouldClose(window)
+
+    fun makeContextCurrent() = GLFW.glfwMakeContextCurrent(window)
+
+    fun enableVSync() = GLFW.glfwSwapInterval(1)
+
+    fun showWindow() = GLFW.glfwShowWindow(window)
+
+    fun configure() {
+        setDefaultWindowHints()
+        setHiddenAfterCreation()
+        setResizable()
+    }
+
+    private fun setDefaultWindowHints() = GLFW.glfwDefaultWindowHints()
+
+    private fun setHiddenAfterCreation() = GLFW.glfwWindowHint(GLFW.GLFW_VISIBLE, GLFW.GLFW_FALSE)
+
+    private fun setResizable() = GLFW.glfwWindowHint(GLFW.GLFW_RESIZABLE, GLFW.GLFW_TRUE)
+
+    private fun getVideoMode() = GLFW.glfwGetVideoMode(getPrimaryMonitor())
+
+    private fun getPrimaryMonitor() = GLFW.glfwGetPrimaryMonitor()
+
+    private fun setWindowPos(x: Int, y: Int) = GLFW.glfwSetWindowPos(window, x, y)
+
+    private fun freeCallbacks() = Callbacks.glfwFreeCallbacks(window)
+
+    private fun destroyWindow() = GLFW.glfwDestroyWindow(window)
+
+    private fun terminate() = GLFW.glfwTerminate()
+
+    companion object {
+        fun setupErrorCallback(ps: PrintStream): GLFWErrorCallback =
+            GLFWErrorCallback.createPrint(ps).set()
+    }
 }
