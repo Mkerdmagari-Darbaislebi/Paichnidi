1a2f9e04d30e0e044d8ed7fe1b41eac6ec6aa331
samKHa92 <samkha@gfmstudio.com>
2022-03-28 12:08:04 +0400

    Update matrix4d class. Extends MatrixBuilder now.

--- src/main/java/math/Matrix4d.java
+++ src/main/java/math/Matrix4d.java
@@ -1,14 +1,16 @@
 package math;
 
-public class Matrix4f {
+import org.jetbrains.annotations.NotNull;
+
+public class Matrix4d extends MatrixBuilder{
 
     private float[][] matrix;
 
-    public Matrix4f() {
+    public Matrix4d() {
 
     }
 
-    public Matrix4f(float[][] m) {
+    public Matrix4d(float[][] m) {
         this.matrix = m;
     }
 
@@ -20,8 +22,8 @@ public class Matrix4f {
         this.matrix = m;
     }
 
-    public static Matrix4f identity4() {
-        Matrix4f im = new Matrix4f();
+    public static Matrix4d identity4() {
+        Matrix4d im = new Matrix4d();
         for (int i = 0; i < 4; i++)
             for (int j = 0; j < 4; j++)
                 if (i == j)
@@ -47,5 +49,39 @@ public class Matrix4f {
             for(int j = 0; j < 4; j++)
                 if(j == columnIndex)
                     this.matrix[i][j] = column[i];
+    }
+
+    @Override
+    public void plusAssign(double scalar) {
+
+    }
+
+    @Override
+    public void minusAssign(double scalar) {
+
+    }
+
+    @NotNull
+    @Override
+    public MatrixBuilder plus(double scalar) {
+        return null;
+    }
+
+    @NotNull
+    @Override
+    public MatrixBuilder minus(double scalar) {
+        return null;
+    }
+
+    @NotNull
+    @Override
+    public MatrixBuilder inc() {
+        return null;
+    }
+
+    @NotNull
+    @Override
+    public MatrixBuilder dec() {
+        return null;
     }
 }
