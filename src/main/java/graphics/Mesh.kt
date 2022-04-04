f829d7291cd02caa222a119d665cc9f601ac2da8
WOTOOOOOO <giorgichavchavadze132@gmail.com>
2022-04-04 17:05:39 +0400

    add some mesh functionality

--- src/main/java/graphics/Mesh.kt
+++ src/main/java/graphics/Mesh.kt
@@ -1,38 +1,47 @@
 package graphics
 
 import org.lwjgl.BufferUtils
+import org.lwjgl.opengl.GL11
 import java.nio.FloatBuffer
 
-// This code will be uncommented once Vertex class is available
-abstract class Mesh(
-//    private var _vertexList : MutableList<Vertex>
-            ){
-//    private var _vertexBuffer : FloatBuffer = BufferUtils.createFloatBuffer(_vertexList.size)
-//    private var _colorBuffer: FloatBuffer = BufferUtils.createFloatBuffer(_vertexList.size)
-//
-//    init {
-//        var positions = mutableListOf<Float>()
-//        var colors = mutableListOf<Float>()
-//        for (vertex in _vertexList){
-//            positions.addAll(vertex.flatten())
-//            colors.addAll(vertex.color.flatten())
-//        }
-//        _vertexBuffer.put(positions.toFloatArray())
-//        _vertexBuffer.position(0)
-//        _colorBuffer.put(positions.toFloatArray())
-//        _colorBuffer.position(0)
-//    }
-//
-//    val vertexBuffer : FloatBuffer get() = _vertexBuffer
-//    val colorBuffer : FloatBuffer get() = _colorBuffer
-//
-//    fun flip(){
-//        _vertexBuffer.flip()
-//        _colorBuffer.flip()
-//    }
-//
-//    fun clear(){
-//        _vertexBuffer.clear()
-//        _colorBuffer.clear()
-//    }
+class Mesh(_vertexList: MutableList<VertexBuilder>) {
+
+    private var _vertexBuffer: FloatBuffer = BufferUtils.createFloatBuffer(_vertexList.size * 2)
+    private var _colorBuffer: FloatBuffer = BufferUtils.createFloatBuffer(_vertexList.size * 4)
+
+    init {
+        val positions = _vertexList.map { it.flatten() }.flatten().toFloatArray()
+        val colors = _vertexList.map { it.flattenColor() }.flatten().toFloatArray()
+
+        _vertexBuffer.put(positions)
+        _vertexBuffer.position(0)
+        _colorBuffer.put(colors)
+        _colorBuffer.position(0)
+    }
+
+    val vertexBuffer: FloatBuffer get() = _vertexBuffer
+    val colorBuffer: FloatBuffer get() = _colorBuffer
+
+    fun flip() {
+        _vertexBuffer.flip()
+        _colorBuffer.flip()
+    }
+
+    fun clear() {
+        _vertexBuffer.clear()
+        _colorBuffer.clear()
+    }
+
+    fun draw(){
+        GL11.glClear(GL11.GL_COLOR_BUFFER_BIT)
+
+        GL11.glEnableClientState(GL11.GL_VERTEX_ARRAY)
+        GL11.glEnableClientState(GL11.GL_COLOR_ARRAY)
+        GL11.glVertexPointer(3, GL11.GL_FLOAT,0 , _vertexBuffer)
+        GL11.glColorPointer(4,GL11.GL_FLOAT,0, _colorBuffer)
+        GL11.glDrawArrays(GL11.GL_TRIANGLES, 0, 3)
+        GL11.glDisableClientState(GL11.GL_VERTEX_ARRAY)
+        GL11.glDisableClientState(GL11.GL_COLOR_ARRAY)
+    }
+
 }
